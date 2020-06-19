289ffe2483ed74b034d840a4125c7d933f3533c6
KhietFarm <weiss1910@gmail.com>
2020-06-19 23:58:21 +0700

    fix giao dien

--- app/src/main/java/com/example/ltdd_th71_nhom5/RegisterActivity.java
+++ app/src/main/java/com/example/ltdd_th71_nhom5/RegisterActivity.java
@@ -26,7 +26,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.mData.child("TaiKhoan").setValue(new User(null, txtHoTen.getText().toString(), txtDiaChi.getText().toString(), "+84" + txtSoDienThoai.getText().toString(), txtEmail.getText().toString(), txtMatKhau.getText().toString(), null));
+                MainActivity.mData.child("TaiKhoan").push().setValue(new User("", txtHoTen.getText().toString(), txtDiaChi.getText().toString(), "+84" + txtSoDienThoai.getText().toString(), txtEmail.getText().toString(), txtMatKhau.getText().toString(), null));
                 Toast.makeText(RegisterActivity.this, "Đăng kí thành công!", Toast.LENGTH_SHORT).show();
                 onBackPressed();
             }
